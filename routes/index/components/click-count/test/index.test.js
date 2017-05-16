a5419de76595ded91f23bbc31741596a548075b1
abiyasa <ilham.suhardi@gmail.com>
2017-05-16 17:03:40 +0200

    Add test for clicking incr. button

--- routes/index/components/click-count/test/index.test.js
+++ routes/index/components/click-count/test/index.test.js
@@ -1,15 +1,30 @@
 var expect = require('chai').expect;
 
-test('should render count', function (context) {
-    var renderResult = context.render({});
+describe('when rendered', function () {
+    test('should render count', function (context) {
+        var renderResult = context.render({});
 
-    var count = renderResult.$('.count').text();
-    expect(count).to.equal('0');
+        var count = renderResult.$('.count').text();
+        expect(count).to.equal('0');
+    });
+
+    test('should render incrementor button', function (context) {
+        var renderResult = context.render({});
+
+        var $button = renderResult.$('.example-button');
+        expect($button).to.have.length(1);
+    });
 });
 
-test('should render incrementor button', function (context) {
+test('should increment counter when incrementor button is clicked', function (context) {
     var renderResult = context.render({});
+    var component = renderResult.component;
 
-    var $button = renderResult.$('.example-button');
-    expect($button).to.have.length(1);
+    var button = component.getEl('increment-button');
+    button.click();
+
+    component.update();
+
+    var counter = component.getEl('counter');
+    expect(counter.innerText).to.equal('1');
 });
